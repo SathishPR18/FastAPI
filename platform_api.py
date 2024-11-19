55ede82501be20ea8e50c0208ad6a943b0000c1f
SathishPR18 <sathishkumar.pr@solidpro-es.com>
2024-11-19 14:00:50 +0530

    Update platform_api.py

--- platform_api.py
+++ platform_api.py
@@ -19,17 +19,17 @@ import json
 # Assuming `json_string` contains your JSON credentials as a string
 json_string = """
 {
-    "type": "service_account",
-    "project_id": "datapopulation-441708",
-    "private_key_id": "0c2d39045082d7703f987ef2096723d554d9a71e",
-    "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC4Hx+aOfho5lBy\\nIkakL4gqbJrx+swyK5pX47PAdvn+7h69ty0/LOChCHQwBN3pTpz2jVkWuvbzI2MT\\ny/qhSqjhtRRytOpdgJ8K0Z7kLcCrxBIAtfRXMcMhmd5qI7Nemp7IXrxw3DdlSZWP\\ni6SNBxYlO48XMGhvPnhUdyk+eMnAnhyaX3LKnTeN7lHZBKtqSkOz0mq72IRDlC4g\\nNu8rFtilkn0XuwDvxunoNlhM9XOZKE5W9cyi68eiaxx0YWHcUjbIynMLbwP5bBjg\\nI0osnmtx6nG9y6C8DnCn3pQoDgIHUxK/PzDkhx34ESeyrwr3Na1BcqGR86ge7N5s\\nMiG1GUtzAgMBAAECggEABMxJYInbSDwoOrEtPbhAGcUZqafldN/Ija855kqcJeFx\\nNAAOjCfQgIqAw+BOC70chUm5Td/4TmG1glfUzFhLuOMZfX6lbB2e2NTIINhFeCjF\\n27CznMoTXVA/iMn+d4TVbsuh7pQRbjZ03I97M4meGe+9BwY3Xh7CcinDsHjjgcMR\\noj5XysQlZ7PjMrxwoPrXhWzQBlNF9Twa7NMHTk2CgkbOyXy0TI3Ebb8MxYXXCBxi\\nTikszNhTLtXGGxBAdwnmdkHRM+aMXGjDV7o13vbkztkYZ9Aqcs98kFm6aQ8Sasf5\\nIuIF6YWLHaM3b9FiWHD/QNMjDHS7G6LI/Rgmg8LxwQKBgQD0NqugEcK+c83OVeVB\\nrrb+xoVGCRLJr/TSS55hnk9XAflScULuVF6DcO5dijFj3Y9jQrXxVshy6mB+Pdx5\\nM5GT/dmGdhi2IE5Fi1I1awXDqF8thEXZumGV/Gy/42JZmnRYXLlNILz7Sk+nV0hR\\nc4tAiD0Z32XPLymHhyDGeYAp0QKBgQDBAf/GkFzcAOLyiN+D2Przj1wwP0PD0kxK\\ncGp5PJV0bOtlo5MWJv6de81B4/qfj7JIwJodfsV9dCvIJBGjnwy5HiJO56DHO08V\\ng1o0DxMkrZh14ddrXgzGMujRCxP1ATYeK8I/dk4rQBbPWTvwKH1PXriBkl9cw+1F\\nojSno9FuAwKBgFYD54EyoEAKc8OoF16CFiw4afqX+YkWM4naXoeNhe5kYJd8ExH0\\nn3F2Vk6V/P1qrTVN6t19Lo7jJGdyjHQYCL9yWqp0cBG94TO3dZYhAt++Lv/OJfgh\\nIHv+c6NGiH5821vAFPgofseXjeSn5m3h33s0QSkauTY3K05z9sRRSSXxAoGBAJDH\\nuB8yIYYi4BsBE3Gq7SyQ6J2Eh4e4E+RvCV+iU5Y/MiFPH8GpRKvLt3/qzRCuURWD\\n7NedRfXClCkQ7W5om7mtYh0AYbmxwrQnkR2mBT0pP0mGPPrcVQbEH6LYBYNvAjKF\\nrrO+Qgrti27EyFJxfPZx3kPMEd7bVfw7HfZ2y14TAoGAUnhcayQa+ipDPxgdjQKX\\n8/fPr+hB289UHhBsW3MyBLgBLArIMpOgtMXbZsaqAQYpyIUbzduPP0AEoARzJSk9\\nnDWUXjqTXA49ovzxuNE9K7Rb+GvtjXBYztEl60jcYmzR8fqLF+6Csxv6Wyixu3ge\\n/mjKveehig8SsXXBs4FzUHw=\\n-----END PRIVATE KEY-----\\n",
-    "client_email": "surya-prakash@datapopulation-441708.iam.gserviceaccount.com",
-    "client_id": "112047189843085462309",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/surya-prakash%40datapopulation-441708.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
+  "type": "service_account",
+  "project_id": "sensordata-442115",
+  "private_key_id": "df896e6f8fb74ee6afbe43b1e51c3cdcd42f1b05",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCX/OSsF8IHYT04\n6YBFigwurU9hfdt3FiGZ6ikT2zQVnc8bGZF+irf+LsigZ2nW8Nqudqqy2Lt0W6G4\nJG1kXpDCxN4QI4BGRVkvLWi7z20oKJbnjRU+3nW4/cSN5X8S4syuaqkx1DUQtfBG\nqK5r/wlbtaRmPL8fOW2Nhh0YOsBXxu9Ow5TK2XFVe+gtItasGQe16qoxloQ8Jf6g\n7G67OCiuDJeC4IO/uk/qORnROm1d+5FN+vGSoLlMaEtRdBvKo5zcD+CNST7DzQSc\nmPXmcNgc8kR6GLstjhYzWeqkAkD7pjJHzgN2kLCdvdPABBMBpFxYYZvuc8n3Klyh\njIYzjnajAgMBAAECggEAAZItLfP5qCdxV0oK7q7oT7NClwH5KHbkHMtTcZJI41L/\ngT/R2eiVTghJVfG+5zFyvj6KxAmpGMWs14ecokGECR1LkbAxgpGUgklJX39CFYtd\n+ccAE/J/QfUZjNOWGRzc1S7tqEzeauvYHYw65Nvv9n9UHLYF5AABSp3fkW4EqeSB\niljUAuLxYXMSWLOGHvEFYgTptG2eac2Qy4530C4DC59XCVCCYSXOvql4wpGnCjJL\nf7PrhfzLlXFY5wfRKHa5hu+mMB2Z6S2Yj2OM1Y32RDPZdeKBgCyHO56v7JpwIIkV\nCSWoRDYOOYq9gs1ipFVlNxkgf9rM9pOZHO1HxMB6/QKBgQDH8O15V1dQU84XXMle\nDxFc/rBSff6ZPO68DMG+u2EuCodq/hHyUczsYqcoYeXTv8/5YPPPHKOBvW2e7tKf\nOTf3cvacFtB2Tpq8GhXp1vqx2hJ3AlrXEfGFbJcMbdUzrhL0b1i9TLte5NDY3TOj\ne7Adunx6/aWW/L+VMYh1bpU/lwKBgQDCmgyFJXt5VlUDqdi0Su1cVVMN18WYwYnc\nTwHj7AnpWDg0TJ3LFPvI1iRCtnxY5Amkl5CmkxYcQ74EsGb4HmLzyuY8Q8K9hyOZ\nv96SsN9pD8v10htWPlt7W5yFTereblCNQSQh15vEOrOavbMey4jMPie0w3C5m7kC\nhwhwrlWi1QKBgD3GogkAxHiMPDwZrhoCu9Go25/RUA3WtsihhGdDDAamuCqFr9PY\nFGHhJVaj0Nf5BvA9VXdjmN1oQut9TNRnYHRzL+EQZ352UPbXdHfYtYKoJ1ZgAuM6\npw4bfBwZ/2rFWRPvJ1Lt12K9fg3TYrYbbFHzIaz6m+Qn2aXmNQxbi3+nAoGAGA06\nJuqvDBwjfcRsSDxKgfL5xOg6P9sL0vLg5O6zeuewaqZdilAZnoT5DlXHoKTunC0v\nb6cWQIAO6D11PI1k5YTaV/B4DTy6pbIVLEQ4GKAfMU66mjoyzFhSTBeJQ9yWkB7Q\n/G3Njr9Cm4l+rfux+Kxl8+2D7SzV/P37iRHC+9ECgYEAiwRc0JXFjH9c8zXlS1tu\nDCEuYn5/wN6z42WOqFLb4FZHDdt0nIawt5UvqnNqqW0aV1YQYYS4WqFbqPAlQjIV\n4cICpLqNIDyZsaRFT7alVPi3yss/PQJ+4UrC98FvL3q1ezfZlJqOPWhN7rSYOhCh\n+NtpysCPKezVqyGdIMra/kY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "testsensordata@sensordata-442115.iam.gserviceaccount.com",
+  "client_id": "104584075051512829103",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/testsensordata%40sensordata-442115.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 }
 """
 
